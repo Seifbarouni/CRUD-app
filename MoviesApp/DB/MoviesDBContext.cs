a7f542ba100c0c55df261194e30e2f21e2b07140
Seifbarouni <seif.barouni11@gmail.com>
2020-10-16 15:58:05 +0100

    Initial commit

---
+++ MoviesApp/DB/MoviesDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.DB
+{
+    public class MoviesDBContext : DbContext
+    {
+        public MoviesDBContext(DbContextOptions<MoviesDBContext>opt):base(opt)
+        {
+
+        }
+    
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
